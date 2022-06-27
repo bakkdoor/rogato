467aab8156cbed8d03f044f49c8193229548b0fb
Christopher Bertels <bakkdoor@flasht.de>
2022-06-27 17:20:36 +0200

    Split up parser tests into more test functions

--- src/tests/fmodl/parser.rs
+++ src/tests/fmodl/parser.rs
@@ -66,14 +66,7 @@ fn module_defs() {
 }
 
 #[test]
-fn expressions() {
-    assert_parse_expr("1", lit(Int64Lit(1)));
-
-    assert_parse_expr(
-        "\"Hello, world!\"",
-        lit(StringLit("Hello, world!".to_string())),
-    );
-
+fn arithmetic_expressions() {
     assert_parse_expr("1+1", sum(lit(Int64Lit(1)), lit(Int64Lit(1))));
 
     assert_parse_expr("5*5", product(lit(Int64Lit(5)), lit(Int64Lit(5))));
@@ -99,17 +92,33 @@ fn expressions() {
         ),
     );
 
+    assert!(parse_expr("(22+)+1").is_err());
+    assert!(parse_expr("1++1").is_err());
+    assert!(parse_expr("3)+1").is_err());
+}
+
+#[test]
+fn literals() {
+    assert_parse_expr("1", lit(Int64Lit(1)));
+
+    assert_parse_expr(
+        "\"Hello, world!\"",
+        lit(StringLit("Hello, world!".to_string())),
+    );
+}
+
+#[test]
+fn fn_calls() {
     assert_parse_expr(
         "add 1 2",
         fn_call("add", vec![lit(Int64Lit(1)), lit(Int64Lit(2))]),
     );
+}
 
+#[test]
+fn op_calls() {
     assert_parse_expr(
         "1 != 2",
         op_call("!=", vec![lit(Int64Lit(1)), lit(Int64Lit(2))]),
     );
-
-    assert!(parse_expr("(22+)+1").is_err());
-    assert!(parse_expr("1++1").is_err());
-    assert!(parse_expr("3)+1").is_err());
 }
