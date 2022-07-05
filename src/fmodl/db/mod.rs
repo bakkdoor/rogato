6890aaa7f9fa078dae70b72d4794e0a177663939
Christopher Bertels <bakkdoor@flasht.de>
2022-07-05 20:51:38 +0200

    Add uuid dependency

--- src/fmodl/db/mod.rs
+++ src/fmodl/db/mod.rs
@@ -1,5 +1,7 @@
 use indradb::{Datastore, Edge, EdgeQuery, Identifier, PropertyValueEdgeQuery, RocksdbDatastore};
+use serde_json::Number;
 use std::{fmt::Debug, path::Path};
+use uuid::Uuid;
 pub mod val;
 
 type DBResult<T> = Result<T, indradb::Error>;
@@ -38,13 +40,35 @@ pub fn index_prop<DB: Datastore>(db: &DB, prop_name: &str) -> DBResult<()> {
     }
 }
 
-fn print_error<E: Debug>(prop_name: &str, error: E) -> E {
-    eprintln!("Failed to index DB property {:?} : {:?}", prop_name, error);
+fn print_error<S: ToString, E: Debug>(prop_name: S, error: E) -> E {
+    eprintln!(
+        "Failed to index DB property {:?} : {:?}",
+        prop_name.to_string(),
+        error
+    );
     error
+}
+
+pub fn create_vertex<DB: Datastore, S: Into<String>>(db: &DB, id_s: S) -> DBResult<Uuid> {
+    let id_string = id_s.into();
+    match Identifier::new(id_string.to_owned()) {
+        Ok(id) => db.create_vertex_from_type(id),
+        Err(error) => {
+            print_error(id_string, error);
+            Err(indradb::Error::Unsupported)
+        }
+    }
 }
 
 pub fn do_stuff<DB: Datastore + Debug>(db: &DB) {
     println!("DB: do stuff with {:?}", db);
     index_prop(db, "testid").unwrap();
-    query_prop(db, "testid", val::string("test-id-value")).unwrap();
+    let res1 = query_prop(db, "testid", val::string("test-id-value")).unwrap();
+    let res2 = query_prop(db, "testid", val::number(Number::from(8540i32))).unwrap();
+    let res3 = query_prop(db, "testid", val::number(Number::from(8540i64))).unwrap();
+    println!("res1 {:?}", res1);
+    println!("res2 {:?}", res2);
+    println!("res3 {:?}", res3);
+    let res4 = create_vertex(db, "testid").unwrap();
+    println!("res4 {:?}", res4);
 }
