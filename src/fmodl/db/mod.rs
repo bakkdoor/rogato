91cd42cb87fce7615b3fac7fabff92d213ca1c4a
Christopher Bertels <bakkdoor@flasht.de>
2022-07-06 05:45:49 +0200

    More DB code cleanup & sample code refactoring

--- src/fmodl/db/mod.rs
+++ src/fmodl/db/mod.rs
@@ -1,8 +1,10 @@
 pub use indradb::{
-    Datastore, Edge, EdgeQuery, Identifier, PropertyValueEdgeQuery, RangeVertexQuery,
-    RocksdbDatastore,
+    BulkInsertItem, Datastore, Edge, EdgeKey, EdgePropertyQuery, EdgeQuery, Identifier,
+    PropertyValueEdgeQuery, RangeVertexQuery, RocksdbDatastore, Vertex,
 };
-use indradb::{EdgeKey, EdgePropertyQuery};
+use indradb::{
+    PropertyPresenceVertexQuery, PropertyValueVertexQuery, VertexPropertyQuery, VertexQuery,
+};
 pub use serde_json::Number;
 use std::{fmt::Debug, path::Path, str::FromStr};
 pub use uuid::Uuid;
@@ -34,40 +36,61 @@ fn print_error<S: ToString, E: Debug>(prop_name: S, error: E) -> E {
 
 pub fn do_stuff<DB: Datastore + Debug>(db: &DB) -> DBResult<()> {
     println!("DB: do stuff with {:?}", db);
-    let vertex_type_id = id("testid");
+    let person_type_id = id("Person");
     let name_prop_id = id("name");
+    let age_prop_id = id("name");
 
     db.index_property(name_prop_id.to_owned())?;
+    db.index_property(age_prop_id.to_owned())?;
 
-    for _ in 0..10000 {
-        let id1 = db.create_vertex_from_type(vertex_type_id.to_owned())?;
-        let id2 = db.create_vertex_from_type(vertex_type_id.to_owned())?;
-        db.create_edge(&EdgeKey::new(id1, vertex_type_id.to_owned(), id2))?;
-        db.set_edge_properties(
-            EdgePropertyQuery::new(
-                EdgeQuery::PropertyValue(PropertyValueEdgeQuery::new(
-                    name_prop_id.to_owned(),
-                    val::string("John Connor"),
-                )),
-                name_prop_id.to_owned(),
+    for i in 0..10000 {
+        let id1 = db.create_vertex_from_type(person_type_id.to_owned())?;
+        let id2 = db.create_vertex_from_type(person_type_id.to_owned())?;
+
+        db.bulk_insert(vec![
+            indradb::BulkInsertItem::VertexProperty(
+                id1.clone(),
+                name_prop_id.clone(),
+                val::string(format!("John Connor {}", i)),
             ),
-            val::string("John Connor"),
-        )?;
+            indradb::BulkInsertItem::VertexProperty(
+                id2.clone(),
+                name_prop_id.clone(),
+                val::string(format!("John Connor {}", i)),
+            ),
+            indradb::BulkInsertItem::VertexProperty(
+                id1.clone(),
+                age_prop_id.clone(),
+                val::number(Number::from(i * 1000)),
+            ),
+            indradb::BulkInsertItem::VertexProperty(
+                id2.clone(),
+                age_prop_id.clone(),
+                val::number(Number::from(i * 9999)),
+            ),
+        ])?;
     }
 
-    let vertices = db.get_vertices(RangeVertexQuery::new().t(vertex_type_id.to_owned()).into())?;
-    let edges = db.get_edges(EdgeQuery::PropertyValue(PropertyValueEdgeQuery::new(
-        name_prop_id.to_owned(),
-        val::string("John Connor"),
-    )));
-
+    let vertices = db.get_vertices(RangeVertexQuery::new().t(person_type_id.to_owned()).into())?;
     println!(
         "Vertex query for type {} gave {} results",
-        vertex_type_id.to_owned().as_str(),
+        person_type_id.to_owned().as_str(),
         vertices.len()
     );
 
-    println!("edges query results {:?}", edges);
+    let vertices_with_name = db.get_vertices(indradb::VertexQuery::PropertyValue(
+        PropertyValueVertexQuery::new(name_prop_id.clone(), val::string("John Connor 1")),
+    ));
+    println!(
+        "vertices_with_name query results:\n{:?}",
+        vertices_with_name
+    );
+
+    let vertex_props = db.get_vertex_properties(VertexPropertyQuery::new(
+        VertexQuery::PropertyPresence(PropertyPresenceVertexQuery::new(age_prop_id.clone())),
+        name_prop_id.clone(),
+    ))?;
+    println!("vertex_props query result count: {}", vertex_props.len());
 
     Ok(())
 }
