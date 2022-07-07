d877aec99bc30451bb3c7b92da66edf6d6e7af17
Christopher Bertels <bakkdoor@flasht.de>
2022-07-07 04:54:36 +0200

    Code cleanup

--- src/fmodl/db/mod.rs
+++ src/fmodl/db/mod.rs
@@ -5,9 +5,8 @@ pub use indradb::{
     PropertyPresenceVertexQuery, PropertyValueEdgeQuery, PropertyValueVertexQuery,
     RangeVertexQuery, RocksdbDatastore, Vertex, VertexPropertyQuery, VertexQuery,
 };
-
 pub use serde_json::Number;
-use std::{fmt::Debug, path::Path, str::FromStr};
+use std::{path::Path, str::FromStr};
 pub use uuid::Uuid;
 
 pub mod val;
@@ -23,14 +22,4 @@ pub fn open<P: AsRef<Path>>(db_path: P) -> DBResult<RocksdbDatastore> {
 
 pub fn id(id: &str) -> Identifier {
     Identifier::from_str(id).unwrap()
-}
-
-#[allow(dead_code)]
-fn print_error<S: ToString, E: Debug>(prop_name: S, error: E) -> E {
-    eprintln!(
-        "Failed to index DB property {:?} : {:?}",
-        prop_name.to_string(),
-        error
-    );
-    error
 }
--- src/fmodl/util.rs
+++ src/fmodl/util.rs
@@ -1,4 +1,4 @@
 pub fn print_error<E: std::fmt::Debug>(error: E) -> E {
-    eprintln!("Error doing DB stuff: {:?}", error);
+    eprintln!("Error: {:?}", error);
     error
 }
