3f520cc1ef15c95fa8b762c545df50a5fe31d337
Christopher Bertels <bakkdoor@flasht.de>
2022-07-10 17:11:31 +0200

    Cleanup ast walker, simplify Visitor trait

--- src/rogato/ast/visitor.rs
+++ src/rogato/ast/visitor.rs
@@ -6,40 +6,10 @@ use super::{
 };
 
 pub trait Visitor {
-    fn program(&mut self, program: &Program) {
-        for node in program.iter() {
-            match &**node {
-                AST::RootComment(c) => self.root_comment(&c),
-                AST::ModuleDef(id, exports) => self.module_def(&id, &exports),
-                AST::FnDef(id, args, body) => self.fn_def(&id, &args, &body),
-                AST::TypeDef(id, type_expr) => self.type_def(&id, &type_expr),
-            }
-        }
-    }
-
     fn root_comment(&mut self, _comment: &String) {}
     fn module_def(&mut self, _id: &Identifier, _exports: &ModuleExports) {}
     fn fn_def(&mut self, _id: &Identifier, _args: &FnDefArgs, _body: &Expression) {}
     fn type_def(&mut self, _id: &Identifier, _type_expr: &TypeExpression) {}
-
-    fn expression(&mut self, expr: &Expression) {
-        match expr {
-            Expression::Commented(c, e) => self.commented(&c, &e),
-            Expression::Lit(l) => self.lit(&l),
-            Expression::Sum(left, right) => self.sum(&left, &right),
-            Expression::Product(left, right) => self.product(&left, &right),
-            Expression::FnCall(id, args) => self.fn_call(&id, &args),
-            Expression::OpCall(id, left, right) => self.op_call(&id, &left, &right),
-            Expression::Var(id) => self.var(&id),
-            Expression::ConstOrTypeRef(id) => self.const_or_type_ref(&id),
-            Expression::EdgeProp(id, edge) => self.edge_prop(&id, &edge),
-            Expression::Let(bindings, expr) => self.let_(&bindings, &expr),
-            Expression::Lambda(args, body) => self.lambda(&args, &body),
-            Expression::Query(query, guards, production) => {
-                self.query(&query, &guards, &production)
-            }
-        }
-    }
 
     fn commented(&mut self, _commented: &String, _expr: &Expression) {}
     fn lit(&mut self, _lit: &Literal) {}
--- src/rogato/ast/walker.rs
+++ src/rogato/ast/walker.rs
@@ -3,70 +3,68 @@ use super::{expression::Expression, visitor::Visitor, AST};
 #[allow(dead_code)]
 pub(crate) fn walk<V: Visitor>(v: &mut V, ast: &AST) {
     match ast {
-        AST::RootComment(c) => v.root_comment(&c),
-        AST::ModuleDef(id, exports) => v.module_def(&id, &exports),
+        AST::RootComment(c) => v.root_comment(c),
+        AST::ModuleDef(id, exports) => v.module_def(id, exports),
         AST::FnDef(id, args, body) => {
-            v.fn_def(&id, &args, &body);
-            walk_expr(v, &body);
+            v.fn_def(id, args, body);
+            walk_expr(v, body);
         }
-        AST::TypeDef(id, type_expr) => v.type_def(&id, &type_expr),
+        AST::TypeDef(id, type_expr) => v.type_def(id, type_expr),
     }
 }
 
 #[allow(dead_code)]
 pub(crate) fn walk_expr<V: Visitor>(v: &mut V, expr: &Expression) {
-    v.expression(&expr);
-
     match expr {
         Expression::Commented(c, expr) => {
-            v.commented(&c, &expr);
-            walk_expr(v, &expr)
+            v.commented(c, expr);
+            walk_expr(v, expr)
         }
-        Expression::Lit(lit_exp) => v.lit(&lit_exp),
+        Expression::Lit(lit_exp) => v.lit(lit_exp),
         Expression::Sum(a, b) => {
-            v.sum(&a, &b);
-            walk_expr(v, &a);
-            walk_expr(v, &b);
+            v.sum(a, b);
+            walk_expr(v, a);
+            walk_expr(v, b);
         }
         Expression::Product(a, b) => {
-            v.product(&a, &b);
-            walk_expr(v, &a);
-            walk_expr(v, &b);
+            v.product(a, b);
+            walk_expr(v, a);
+            walk_expr(v, b);
         }
         Expression::FnCall(id, args) => {
-            v.fn_call(&id, &args);
+            v.fn_call(id, args);
             for a in args.iter() {
-                walk_expr(v, &a);
+                walk_expr(v, a);
             }
         }
         Expression::OpCall(id, left, right) => {
-            v.op_call(&id, &left, &right);
-            walk_expr(v, &left);
-            walk_expr(v, &right);
+            v.op_call(id, left, right);
+            walk_expr(v, left);
+            walk_expr(v, right);
         }
-        Expression::Var(id) => v.var(&id),
-        Expression::ConstOrTypeRef(id) => v.const_or_type_ref(&id),
-        Expression::EdgeProp(id, edge) => v.edge_prop(&id, &edge),
+        Expression::Var(id) => v.var(id),
+        Expression::ConstOrTypeRef(id) => v.const_or_type_ref(id),
+        Expression::EdgeProp(id, edge) => v.edge_prop(id, edge),
         Expression::Let(bindings, body) => {
-            v.let_(&bindings, &body);
-            v.expression(&body);
+            v.let_(bindings, body);
+            walk_expr(v, body);
             for (_id, val) in bindings.iter() {
-                walk_expr(v, &val);
+                walk_expr(v, val);
             }
         }
         Expression::Lambda(args, body) => {
-            v.lambda(&args, &body);
-            walk_expr(v, &body);
+            v.lambda(args, body);
+            walk_expr(v, body);
         }
         Expression::Query(bindings, guards, production) => {
-            v.query(&bindings, &guards, &production);
+            v.query(bindings, guards, production);
             for binding in bindings.iter() {
                 walk_expr(v, binding.value());
             }
             for g in guards.iter() {
-                walk_expr(v, &g);
+                walk_expr(v, g);
             }
-            walk_expr(v, &production);
+            walk_expr(v, production);
         }
     }
 }
