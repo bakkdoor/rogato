7b0b0944543905c68100de378eeb16a4e0aa2fa7
Christopher Bertels <bakkdoor@flasht.de>
2022-07-11 05:07:56 +0200

    Add define_var and lookup_var to Environment

--- src/rogato/interpreter/environment.rs
+++ src/rogato/interpreter/environment.rs
@@ -28,4 +28,21 @@ impl<'a> Environment<'a> {
             modules: HashMap::new(),
         }
     }
+
+    #[allow(dead_code)]
+    pub fn define_var(&'a mut self, id: &Identifier, val: Box<Expression>) -> &'a mut Self {
+        self.variables.insert(id.clone(), val);
+        self
+    }
+
+    #[allow(dead_code)]
+    pub fn lookup_var(&'a self, id: &Identifier) -> Option<&'a Expression> {
+        match self.variables.get(id) {
+            Some(expr) => Some(expr),
+            None => match self.parent {
+                Some(parent_env) => parent_env.lookup_var(id),
+                None => None,
+            },
+        }
+    }
 }
