f8fd8c2019b21e275ea6bae79c3ecea9b88c5fad
Christopher Bertels <bakkdoor@flasht.de>
2022-08-29 04:45:18 +0200

    Improve EvalError Display formatting

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -20,37 +20,37 @@ use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq, Eq, Clone)]
 pub enum EvalError {
-    #[error("Unknown evaluation error: {0}")]
+    #[error("Unknown error: {0}")]
     Unknown(String),
 
-    #[error("EvalError: QueryError: {0:?}")]
+    #[error("{0}")]
     QueryFailed(QueryError),
 
-    #[error("EvalError: FnCall argument error: {0}")]
+    #[error("{0}")]
     FnCallArgumentError(Box<EvalError>),
 
-    #[error("EvalError: Constant or Type not found: {0}")]
+    #[error("Constant or Type not found: {0}")]
     ConstOrTypeNotFound(Identifier),
 
-    #[error("EvalError: Operator not defined: {0}")]
+    #[error("Operator not defined: {0}")]
     OperatorNotDefined(Identifier),
 
-    #[error("EvalError: Var not defined: {0}")]
+    #[error("Var not defined: {0}")]
     VarNotDefined(Identifier),
 
-    #[error("EvalError: Function not defined: {0}")]
+    #[error("Function not defined: {0}")]
     FunctionNotDefined(Identifier),
 
-    #[error("EvalError: Function arity mismatch for {0} : Expected: {1} but got: {2}")]
+    #[error("Function arity mismatch for {0} : Expected: {1} but got: {2}")]
     FunctionArityMismatch(Identifier, usize, usize),
 
-    #[error("EvalError: Lambda arity mismatch: Expected: {0} but got: {1}")]
+    #[error("Lambda arity mismatch: Expected: {0} but got: {1}")]
     LambdaArityMismatch(usize, usize),
 
-    #[error("EvalError: {0}")]
+    #[error("{0}")]
     NativeFnFailed(NativeFnError),
 
-    #[error("EvalError: IfElse condition is not a Bool value: {0}")]
+    #[error("IfElse condition is not a Bool value: {0}")]
     IFElseConditionNotBool(ValueRef),
 }
 
