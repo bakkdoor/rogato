2aa48efeeaa62206872bf6dd96bc6a34c3afeff7
Christopher Bertels <bakkdoor@flasht.de>
2022-10-19 00:34:33 +0200

    Add LLVM builder and JIT execution engine to Compiler

--- compiler/src/ast/fn_def.rs
+++ compiler/src/ast/fn_def.rs
@@ -1,8 +1,42 @@
 use crate::{Compile, Compiler, CompilerResult};
+use inkwell::types::BasicMetadataTypeEnum;
 use rogato_common::ast::fn_def::FnDef;
 
 impl Compile<()> for FnDef {
-    fn compile(&self, _compiler: &mut Compiler) -> CompilerResult<()> {
+    fn compile(&self, c: &mut Compiler) -> CompilerResult<()> {
+        let f32_type = c.context.f32_type();
+        let fn_type = f32_type.clone().fn_type(
+            &[
+                BasicMetadataTypeEnum::FloatType(f32_type),
+                BasicMetadataTypeEnum::FloatType(f32_type),
+                BasicMetadataTypeEnum::FloatType(f32_type),
+            ],
+            false,
+        );
+        let func = c.module.add_function(self.id(), fn_type, None);
+
+        let basic_block = c.context.append_basic_block(func, self.id());
+        c.builder.position_at_end(basic_block);
+
+        let x = func.get_nth_param(0).unwrap().into_float_value();
+        let y = func.get_nth_param(1).unwrap().into_float_value();
+        let z = func.get_nth_param(2).unwrap().into_float_value();
+
+        let sum = c.builder.build_float_add(x, y, "sum1");
+        let sum = c.builder.build_float_add(sum, z, "sum2");
+
+        c.builder.build_return(Some(&sum));
+
+        unsafe {
+            // c.execution_engine.get_function(self.id().as_str()).ok();
+            let test_fn = c
+                .execution_engine
+                .get_function::<unsafe extern "C" fn(f64, f64, f64) -> f64>(self.id().as_str())
+                .unwrap();
+            let return_value = test_fn.call(1.1f64, 2.22f64, 3.333f64);
+            assert_eq!(return_value, 6.653f64);
+        }
+
         todo!()
     }
 }
--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,7 +1,10 @@
 #[cfg(test)]
 pub mod tests;
 
-use inkwell::{context::Context, module::Module};
+use inkwell::{
+    builder::Builder, context::Context, execution_engine::ExecutionEngine, module::Module,
+    OptimizationLevel,
+};
 use rogato_common::ast::expression::Expression;
 use thiserror::Error;
 
@@ -22,21 +25,39 @@ pub enum CodeGenError {
 #[derive(Debug)]
 pub struct Compiler<'ctx> {
     context: &'ctx Context,
-
-    #[allow(dead_code)]
     module: Module<'ctx>,
+    builder: Builder<'ctx>,
+    #[allow(dead_code)]
+    execution_engine: ExecutionEngine<'ctx>,
 }
 
 impl<'ctx> Compiler<'ctx> {
     pub fn new(context: &'ctx Context, module: Module<'ctx>) -> Self {
-        Self { context, module }
+        let execution_engine = Compiler::execution_engine(&module);
+        Self {
+            context,
+            module,
+            builder: context.create_builder(),
+            execution_engine,
+        }
     }
 
     pub fn new_with_module_name<S: ToString>(context: &'ctx Context, module_name: S) -> Self {
+        let builder = context.create_builder();
+        let module = context.create_module(module_name.to_string().as_str());
+        let execution_engine = Compiler::execution_engine(&module);
         Self {
             context,
-            module: context.create_module(module_name.to_string().as_str()),
+            module,
+            builder,
+            execution_engine,
         }
+    }
+
+    pub fn execution_engine(module: &Module<'ctx>) -> ExecutionEngine<'ctx> {
+        module
+            .create_jit_execution_engine(OptimizationLevel::None)
+            .unwrap()
     }
 
     pub fn new_context() -> Context {
